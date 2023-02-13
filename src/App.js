3c203611d4c612a45a32059bc97279e13a64681e
hana <hanasabih@gmail.com>
2023-02-13 21:39:22 +0200

    add component calss based

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import './index.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App ">
-      <Calculator />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = null;
+  }
 
+  render() {
+    return (
+      <div className="App ">
+        <Calculator />
+      </div>
+    );
+  }
+}
 export default App;
