e90a80e34aedc6264cb6eccfd7e6bb33a2891a00
hana <hanasabih@gmail.com>
2023-02-16 08:48:10 +0200

    full website with router

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Stack } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import calculate from '../logic/calculate';
 
@@ -18,44 +20,53 @@ const Calculator = () => {
   const { total, next, operation } = state;
 
   return (
+    <Container className=" mx-auto homeCalc">
+      <Row>
+        <Col xs={12} md={4}>
+          <div className=" mt-5 "><h1>Lets do some Math!</h1></div>
+        </Col>
+        <Col xs={12} md={8}>
+          <Container className=" mt-5  pb-3 ">
 
-    <Container className="w-25 mt-5  ">
-      <Stack direction="horizontal" className="mx-auto bg-secondary resultContainer">
-        <div className="text-end w-100 bg-secondary text-white px-3">
-          <span>{total}</span>
-          <span>{operation}</span>
-          <span>{next}</span>
-        </div>
-      </Stack>
-      <Stack direction="horizontal" className="mx-auto">
-        <button type="button" name="AC" onClick={clickBtn}>AC</button>
-        <button type="button" name="+/-" onClick={clickBtn}>+/-</button>
-        <button type="button" name="%" onClick={clickBtn}>%</button>
-        <button type="button" name="÷" className="orangeBtn" onClick={clickBtn}>÷</button>
-      </Stack>
-      <Stack direction="horizontal" className="mx-auto">
-        <button type="button" name="7" onClick={clickBtn}>7</button>
-        <button type="button" name="8" onClick={clickBtn}>8</button>
-        <button type="button" name="9" onClick={clickBtn}>9</button>
-        <button type="button" name="x" className="orangeBtn" onClick={clickBtn}>x</button>
-      </Stack>
-      <Stack direction="horizontal" className="mx-auto">
-        <button type="button" name="4" onClick={clickBtn}>4</button>
-        <button type="button" name="5" onClick={clickBtn}>5</button>
-        <button type="button" name="6" onClick={clickBtn}>6</button>
-        <button type="button" name="-" className="orangeBtn" onClick={clickBtn}>-</button>
-      </Stack>
-      <Stack direction="horizontal" className="mx-auto">
-        <button type="button" name="1" onClick={clickBtn}>1</button>
-        <button type="button" name="2" onClick={clickBtn}>2</button>
-        <button type="button" name="3" onClick={clickBtn}>3</button>
-        <button type="button" name="+" className="orangeBtn" onClick={clickBtn}>+</button>
-      </Stack>
-      <Stack direction="horizontal" className="mx-auto">
-        <button type="button" name="0" className="col-md-6" onClick={clickBtn}>0</button>
-        <button type="button" name="." onClick={clickBtn}>.</button>
-        <button type="button" name="=" className="orangeBtn" onClick={clickBtn}>=</button>
-      </Stack>
+            <Stack direction="horizontal" className="mx-auto bg-secondary resultContainer fs-1">
+              <div className="text-end w-100 bg-secondary text-white px-3">
+                <span>{total}</span>
+                <span>{operation}</span>
+                <span>{next}</span>
+              </div>
+            </Stack>
+            <Stack direction="horizontal" className="mx-auto fs-3">
+              <button type="button" name="AC" onClick={clickBtn}>AC</button>
+              <button type="button" name="+/-" onClick={clickBtn}>+/-</button>
+              <button type="button" name="%" onClick={clickBtn}>%</button>
+              <button type="button" name="÷" className="orangeBtn" onClick={clickBtn}>÷</button>
+            </Stack>
+            <Stack direction="horizontal" className="mx-auto fs-3">
+              <button type="button" name="7" onClick={clickBtn}>7</button>
+              <button type="button" name="8" onClick={clickBtn}>8</button>
+              <button type="button" name="9" onClick={clickBtn}>9</button>
+              <button type="button" name="x" className="orangeBtn" onClick={clickBtn}>x</button>
+            </Stack>
+            <Stack direction="horizontal" className="mx-auto fs-3">
+              <button type="button" name="4" onClick={clickBtn}>4</button>
+              <button type="button" name="5" onClick={clickBtn}>5</button>
+              <button type="button" name="6" onClick={clickBtn}>6</button>
+              <button type="button" name="-" className="orangeBtn" onClick={clickBtn}>-</button>
+            </Stack>
+            <Stack direction="horizontal" className="mx-auto fs-3">
+              <button type="button" name="1" onClick={clickBtn}>1</button>
+              <button type="button" name="2" onClick={clickBtn}>2</button>
+              <button type="button" name="3" onClick={clickBtn}>3</button>
+              <button type="button" name="+" className="orangeBtn" onClick={clickBtn}>+</button>
+            </Stack>
+            <Stack direction="horizontal" className="mx-auto fs-3">
+              <button type="button" name="0" className="col-6 " onClick={clickBtn}>0</button>
+              <button type="button" name="." onClick={clickBtn}>.</button>
+              <button type="button" name="=" className="orangeBtn" onClick={clickBtn}>=</button>
+            </Stack>
+          </Container>
+        </Col>
+      </Row>
     </Container>
   );
 };
---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+// import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Navbars = () => (
+  <>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Math magicians</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto justify-content-end flex-grow-1 pe-3">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Calculator">Calculator</Nav.Link>
+            <Nav.Link href="/Quote">Quote</Nav.Link>
+          </Nav>
+
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Outlet />
+  </>
+  //   <>
+  //     <nav>
+  //       <ul>
+  //         <li>
+  //           <Link to="/">Home</Link>
+  //         </li>
+  //         <li>
+  //           <Link to="/Calculator">Calculator</Link>
+  //         </li>
+  //         <li>
+  //           <Link to="/Quote">Quote</Link>
+  //         </li>
+  //       </ul>
+  //     </nav>
+
+//     <Outlet />
+//   </>
+);
+
+export default Navbars;
